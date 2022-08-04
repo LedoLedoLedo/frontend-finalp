ac45721bcbca26e8fc3a89eae98a9839f2b0bc68
Alejandro Fernandez-Sarasua <alfea1983@gmail.com>
2022-08-04 16:42:05 -0400

    about updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 const About = ()=>{
 return (
 <div>
-<h2> Welcome Y'all lets us introduce you to our services </h2>
+<h2> Welcome Y'all lets us introduce you to our services. </h2>
+  <h3>We are the most locally based events search in USA, allowing you to get detail information on your zipcode.<br/>
+  We have now added one of our more liked features Which is our Gig Posting Board ! There you can make sure you catch all extremely local events</h3>
+
+
 </div>
 )
 }
